a362484708d8c37c623f5c941a0b01bf801e1256
XiaoVLiu <xwliu@thoughtworks.com>
2016-08-01 15:25:56 +0800

    [liu xiaowei]modify main menu string as a field

--- src/com/twu/biblioteca/source/MainMenuString.java
+++ src/com/twu/biblioteca/source/MainMenuString.java
@@ -1,9 +1,11 @@
 package com.twu.biblioteca.source;
 
 public class MainMenuString {
+
+    private static final String mainMenuString =
+            String.format("************Main Menu************\n1. List Books\n*********************************\n");
+
     public static String getString() {
-        return "************Main Menu************\n" +
-                "1. List Books\n" +
-                "*********************************\n";
+        return mainMenuString;
     }
 }
